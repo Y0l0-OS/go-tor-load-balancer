426463ceeff4b81534f86ad148c1de7f20242e5d
B³ŋ¢ħøN4uŧ <github@b1nch0.us.to>
2022-12-16 02:40:21 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,13 @@
 mkdir /tmp/screens
 chmod 755 /tmp/screens
- for userno in $(seq 1 6); do screen -dmS tor$userno /bin/ash -c '(echo "DataDirectory /dev/shm/tor"'$userno';echo "ControlPort 2000'$userno'";echo "SOCKSPort 1000'$userno'")|tor -f /dev/stdin';done
+phrase=$RANDOM"_asd_"$RANDOM"_wsx_"$RANDOM
+pswhash=$(tor  --hash-password  "$phrase"|grep -v "You are running Tor as ro")
+echo "control pass is $phrase"
+echo "$phrase" > /dev/shm/.ctrlpsw
+[[ -z "$TORCOUNT" ]] && TORCOUNT=6
+echo $TORCOUNT > /tmp/.TOR_COUNT
+mkdir /tmp/.tordata
+ for userno in $(seq 1 $TORCOUNT); do screen -dmS tor$userno /bin/ash -c '(echo "DataDirectory /tmp/.tordata/tor"'$userno';echo "ControlPort 2000'$userno'";echo "SOCKSPort 1000'$userno'";echo "HashedControlPassword '$pswhash'")|tor -f /dev/stdin';done
 linecnt=0
 conncnt=0
 go-dispatch-proxy -lhost 0.0.0.0 -lport 9050 -tunnel $(for m in $(seq 1 6 );do echo 127.0.0.1:1000$m@3;done) 2>&1|while read line; do 
